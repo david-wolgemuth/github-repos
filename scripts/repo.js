b2cbd0154204c3d1cce0e9c99cd45cb0f87f418a
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-07-22 15:57:21 -0500

    can retrieve number of open pull requests

--- scripts/repo.js
+++ scripts/repo.js
@@ -15,19 +15,42 @@ class Repo
   {
     this._repoName = repoName;
     this._repoData = null;
+    this._pulls    = [];
+    this.numberOfPulls = null;
   }
   fetch()
   {
     if (DEVELOPMENT) {
       this._repoData = window.SAMPLE_REPO;
+      this.numberOfPulls = 12;
       return Promise.resolve(null);
     }
     return fetch(`${API_ROOT}/repos/${this._repoName}`, FETCH_OPTIONS)
     .then(response => response.json())
-    .then(json => {
-      console.log(JSON.stringify(json));
-      return null;
+    .then(data => {
+      this._repoData = data;
+      return this._getNumberOfPulls();
     });
   }
-
+  _getNumberOfPulls ()
+  {
+    return fetch(`${API_ROOT}/repos/${this._repoName}/pulls`, FETCH_OPTIONS)
+    .then(response => {
+      const pagString = response.headers.get('Link');
+      const reg = /page=(\d)/g;
+      if (reg.exec(pagString)) {
+        let numberOfPages = parseInt(reg.exec(pagString)[1]);
+        this.numberOfPulls = (numberOfPages-1) * 30;
+        return fetch(`${API_ROOT}/repos/${this._repoName}/pulls?page=${numberOfPages}`)
+        .then(response => response.json()).then(pulls => {
+          this.numberOfPulls += pulls.length;
+        });
+      } else {
+        return response.json().then(pulls => {
+          this.numberOfPulls = pulls.length;
+          return null;
+        });
+      }
+    });
+  }
 }
